3c7d5d412c2e355756d05b40cf8be39774d4b344
Md. Rasel Hossain <rasel.as.cse@gmail.com>
2021-03-25 22:31:22 +0600

    load data from database and show on the ui

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ const app = express()
 const port = 3000
 
 const password = "oGwFfVUWCZBrI5zK";
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+
+const cors = require('cors')
+app.use(cors())
+
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://organicUser:oGwFfVUWCZBrI5zK@cluster0.muahx.mongodb.net/organicdb?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -25,6 +30,12 @@ client.connect(err => {
             })
 
     });
+    app.get("/products", (req, res)=>{
+        productCollection.find({})
+        .toArray((err, documents)=>{
+            res.send(documents)
+        })
+    })
     //   client.close();
 })
 

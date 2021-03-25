6d52fc2e7e069ef5c1aa166ab9114cd35f76a65b
Md. Rasel Hossain <rasel.as.cse@gmail.com>
2021-03-25 22:00:55 +0600

    send data to database from user

--- index.js
+++ index.js
@@ -3,21 +3,31 @@ const app = express()
 const port = 3000
 
 const password = "oGwFfVUWCZBrI5zK";
-
+const bodyParser = require('body-parser');
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://organicUser:oGwFfVUWCZBrI5zK@cluster0.muahx.mongodb.net/organicdb?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.sendFile(__dirname + '/index.html')
 })
 
 client.connect(err => {
-  const collection = client.db("organicdb").collection("products");
-  console.log('database connected');
-  client.close();
-});
+    const productCollection = client.db("organicdb").collection("products");
+    app.post("/addProduct", (req, res) => {
+        const product = req.body;
+        productCollection.insertOne(product)
+            .then(function (result) {
+                console.log('success');
+                res.send('success');
+            })
+
+    });
+    //   client.close();
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
